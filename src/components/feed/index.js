fdb99d09f847b3ed7f389b024f9ab983de1718fe
Ali <ali@mymantl.app>
2026-03-27 16:24:26 +0000

    Remove dead feed components: EpisodeCard, BadgeCompleteCard, TrendingCard, UpNextCard, RandomPickCard + .bak

--- src/components/feed/index.js
+++ src/components/feed/index.js
@@ -1,11 +1,6 @@
 export { default as LogCard } from "./LogCard";
 export { default as BrowseCard } from "./BrowseCard";
 export { default as PodcastCard } from "./PodcastCard";
-export { default as EpisodeCard } from "./EpisodeCard";
 export { default as BadgeCard } from "./BadgeCard";
-export { default as BadgeCompleteCard } from "./BadgeCompleteCard";
-export { default as TrendingCard } from "./TrendingCard";
-export { default as UpNextCard } from "./UpNextCard";
-export { default as RandomPickCard } from "./RandomPickCard";
 export { default as EmptyFeed } from "./EmptyFeed";
 export { FeedCard } from "./FeedPrimitives";
